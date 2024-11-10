d04099a2d99f9c309f0ec36528f5aed6c93bd8a4
soyvillareal <sebastiangarces152@gmail.com>
2024-11-10 07:03:33 -0500

    Deleted dist and i did add exports to transformer

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Radial } from "./lib/Radial";
-export { Circle, type ConfigCircle } from "./lib/class/Shapes/Circle";
-export { Rect, type ConfigRect } from "./lib/class/Shapes/Rect";
-export { Triangle, type ConfigTriangle } from "./lib/class/Shapes/Triangle";
-export { Line, type ConfigLine } from "./lib/class/Shapes/Line";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Radial } from "./lib/Radial";
-export { Circle, type ConfigCircle } from "./lib/class/Shapes/Circle";
-export { Rect, type ConfigRect } from "./lib/class/Shapes/Rect";
-export { Triangle, type ConfigTriangle } from "./lib/class/Shapes/Triangle";
-export { Line, type ConfigLine } from "./lib/class/Shapes/Line";
+export { Circle, type ConfigCircle } from "./lib/class/shapes/Circle";
+export { Rect, type ConfigRect } from "./lib/class/shapes/Rect";
+export { Triangle, type ConfigTriangle } from "./lib/class/shapes/Triangle";
+export { Line, type ConfigLine } from "./lib/class/shapes/Line";
+export { Transformer, type ConfigTransformer } from "./lib/class/utils/Transformer";
